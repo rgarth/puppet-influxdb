e77edaaeb4d02fa073fc9f98f9f35cc8d1c7ea34
Dennis de Greef <github@link0.net>
2016-10-09 13:08:17 +0200

    Notify apt-get update on adding repository

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,9 +5,9 @@ class influxdb::install inherits influxdb {
 
     if (!defined(Apt::Source['influxdb'])) {
       apt::source { 'influxdb':
-        location => 'https://repos.influxdata.com/debian',
-        repos    => 'stable',
-        key      => {
+        location   => 'https://repos.influxdata.com/debian',
+        repos      => 'stable',
+        key        => {
           'id'     => '05CE15085FC09D18E99EFB22684A14CF2582E0C5',
           'server' => 'pgp.mit.edu',
         },
@@ -18,6 +18,7 @@ class influxdb::install inherits influxdb {
       ensure  => $influxdb::package_ensure,
       name    => $influxdb::package_name,
       require => Apt::Source['influxdb'],
+      notify  => Exec['apt_update'],
     }
   }
 
